a4a3988dcc76ce4e8d6afee445f5642419a83049
Taylor Thomson <thomtay9@gmail.com>
2019-01-26 01:02:02 +1300

    Created Indexer and IndexFile cass

---
+++ Indexer/Indexer/IndexFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    class IndexFile
+    {
+        private int nameLength;
+        private int size;
+        private string name;
+        private string homeDir;
+
+        public IndexFile(string name, string homeDir)
+        {
+            this.name = name;
+            this.homeDir = homeDir;
+        }
+
+        public void getLength()
+        {
+            getNameLenght();
+        }
+
+        private void getNameLenght()
+        {
+            nameLength = name.LastIndexOf(".");
+        }
+    }
+}
